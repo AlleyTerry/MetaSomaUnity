e823462d4f923e79d528c09aaa6383371d0294fc
VivianC <37090776+myamyanya@users.noreply.github.com>
2024-11-21 00:05:07 -0500

    Interaction WIP

---
+++ MetaSomaUnity/Assets/Scripts/ImerisInteraction.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImerisInteraction : MonoBehaviour
+{
+    // VARIABLES
+    [SerializeField] private bool isOverlapping = false;
+    public GameObject overlappedItem;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOverlapping &&
+            (Input.GetButtonDown("Interact") || Input.GetKeyDown(KeyCode.E)))
+        {
+            Debug.Log("Interacting with " + overlappedItem.name);
+            Destroy(overlappedItem);
+        }
+    }
+    
+    // OVERLAPPING WITH INTERACTABLE ITEM
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("InteractableItem"))
+        {
+            isOverlapping = true;
+            overlappedItem = other.gameObject;
+        }
+    }
+    
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("InteractableItem"))
+        {
+            isOverlapping = true;
+            overlappedItem = other.gameObject;
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("InteractableItem"))
+        {
+            isOverlapping = false;
+            overlappedItem = null;
+        }
+    }
+}
