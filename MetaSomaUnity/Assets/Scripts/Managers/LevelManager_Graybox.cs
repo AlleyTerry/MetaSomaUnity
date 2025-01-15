f423f4ae11e5644adfbed77ab9d63fe9173e11e7
VivianC <37090776+myamyanya@users.noreply.github.com>
2025-01-14 22:10:40 -0500

    Optimizing LevelManager for garybox scene

--- MetaSomaUnity/Assets/Scripts/Managers/LevelManager_Graybox.cs
+++ MetaSomaUnity/Assets/Scripts/Managers/LevelManager_Graybox.cs
@@ -7,7 +7,14 @@ public class LevelManager_Graybox : LevelManagerBase
    protected override void Start()
    {
       base.Start();
+      
       ImerisMovement.instance.currentState = new BeforeAnyEvolutionState(SubState.Healthy);
+   }
+
+   public override void Initialize()
+   {
+      base.Initialize();
+      
       GameManager.instance.HUD.SetActive(true);
    }
 }
