5b3c343778b3bc7a75bba1dd6cd40468f9f6a425
VivianC <37090776+myamyanya@users.noreply.github.com>
2025-02-13 15:23:23 -0500

    Update InteractableItem_MultipleSentences.cs

--- MetaSomaUnity/Assets/Scripts/InteractableItem_MultipleSentences.cs
+++ MetaSomaUnity/Assets/Scripts/InteractableItem_MultipleSentences.cs
@@ -11,9 +11,15 @@ public class InteractableItemMultipleSentences : InteractableItemBase
         if (other.CompareTag("Player"))
         {
             GameManager.instance.FreezeControls();
-            
+            StartCoroutine(DisableCollider());
         }
     }
-    
-    
+
+    IEnumerator DisableCollider()
+    {
+        yield return new WaitForSeconds(0.95f);
+        gameObject.GetComponent<Collider>().enabled = false; // DISABLE COLLIDER
+        isOverlapping = false;
+        GetComponentInChildren<Animator>().Play("EyeIndicator_BNW_backward");
+    }
 }
