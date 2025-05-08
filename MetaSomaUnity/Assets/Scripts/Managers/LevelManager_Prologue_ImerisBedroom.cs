e29417f6ddfb28062bc81ef5bc334e8ef0ff9d5b
VivianC <37090776+myamyanya@users.noreply.github.com>
2025-05-08 17:49:57 -0400

    fixing some tiny bugs

--- MetaSomaUnity/Assets/Scripts/Managers/LevelManager_Prologue_ImerisBedroom.cs
+++ MetaSomaUnity/Assets/Scripts/Managers/LevelManager_Prologue_ImerisBedroom.cs
@@ -45,6 +45,8 @@ public class LevelManager_Prologue_ImerisBedroom : LevelManagerBase
         PortraitZoom.SetActive(false);
         //play background music
         //gameObject.GetComponent<AudioManager>().PlayMusic("Memories");
+        
+        DialogueManager.instance.UpdateDialogueText();
     }
     // Start is called before the first frame update
     void Start()
